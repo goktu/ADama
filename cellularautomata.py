6a097e5e5cbc2154a0816a24cd5da83526e6b726
Goktu <islamoglu@itu.edu.tr>
2020-07-27 06:04:56 +0300

    Ready for Release
    Cotangent function and critical points added

--- cellularautomata.py
+++ cellularautomata.py
@@ -1,27 +1,51 @@
 #!/usr/bin/env python
 
-""" ADama: Checkerboard Formation Ising Model """
+<<<<<<< HEAD
+""" Coupled Linear Ising Chain """
 
-# Author: Goktug Islamoglu
-# 2018- (c) Copyright by Goktug Islamoglu
-# License: GPL
-# Email: islamoglu@itu.edu.tr
+__author__ = "Goktug Islamoglu"
+__copyright__ = "Copyright 2020, Goktug Islamoglu"
+__credits__ = "Goktug Islamoglu"
+__license__ = "GPL"
+__version__ = "1.0.1"
+__maintainer__ = "Goktug Islamoglu"
+__email__ = "goktugislamoglu@gmail.com"
+__status__ = "Prototype"
+
+#    GNU Public License
+
+#    GNU_PUBLIC_LICENSE
+#    PyClustering is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+    
+#    PyClustering is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+    
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+=======
+""" ADama: Checkerboard Formation Ising Model """
+>>>>>>> f1a94baf9f74450a6866639319fb04ff23d16589
+
 
 ##
-# Built on: PyCX 0.3 Realtime Visualization Template
-#
+#Built on: PyCX 0.3 Realtime Visualization Template
+#    
 # Source:
 # http://pycx.sourceforge.net/
+#  
+# Written by:
+# Chun Wong
+# email@chunwong.net
 #
-# PyCX 0.32
-# Complex Systems Simulation Sample Code Repository
-# 2008-2016 (c) Copyright by Hiroki Sayama
-# 2012 (c) Copyright by Chun Wong & Hiroki Sayama
-#         Original GUI module and simulation models
-# 2013 (c) Copyright by Przemyslaw Szufel & Bogumil Kaminski
-#         Extensions to GUI module, some revisions
-# All rights reserved.
-#
+# Revised by:
+# Hiroki Sayama
+# sayama@binghamton.edu
+
 
 import matplotlib
 matplotlib.use('TkAgg')
@@ -29,33 +53,50 @@ matplotlib.use('TkAgg')
 from pylab import *
 import numpy as np
 
-L = 100  # size of 2D lattice: LxL
+L = 100  # size of space: LxL
 
-p = 5 / 9  # Approximation to upper bound of 2nd order phase transition
+#ferrimagnetic-transition phase
+#p = 0.5276973969625715285724233433631805779688537906314195417222751595 / 0.662743419349181580974742097109252907056233549115022417520392534990971853086
 
-# p = 4/9 #Approximation to lower bound of 2nd order phase transition
+#Ising critical point best approximation
+#p = 0.66274341934918158097474209710925290*0.66274341934918158097474209710925290
 
-# p = float(0.5 + (1 / (2 * sqrt(2)))) #upper bound of 1st order phase transition
-# p = 0.8536
+#p = float(0.5 + (1 / (2 * sqrt(2)))) # ferrimagnetic probability - coupled
+#p = 0.8536 #paramagnetic transition
 
-# p = float(0.5 - (1 / (2 * sqrt(2)))) #lower bound of 1st order phase transition
-# p = 0.1464466
+#p = float(0.5 - (1 / (2 * sqrt(2)))) # antiferromagnetic probability - coupled
+#p = 0.1464466 #paramagnetic transition
 
+p = np.log(1+sqrt(2))/2 + 0.5*(sqrt(2)-1) #maximum 
+
+<<<<<<< HEAD
+=======
 # p = (cos(π/8)+ cos^2(π/8)) /4 # best approximation to Onsager's 2D Ising Model Curie point
 
 ##
+>>>>>>> f1a94baf9f74450a6866639319fb04ff23d16589
 # initializing randomly assigned states with probability p
 
 
 def init():
+<<<<<<< HEAD
+    global c, nc, slope0, slope1, delta, o
+=======
     global c, nc
+>>>>>>> f1a94baf9f74450a6866639319fb04ff23d16589
     c = zeros([L, L])
     for x in xrange(L):
         for y in xrange(L):
             c[x, y] = 1 if random() < p else 0
     nc = zeros([L, L])
+<<<<<<< HEAD
+    o = 0
+    slope0 = []
+    slope1 = []
+    delta = []
+=======
+>>>>>>> f1a94baf9f74450a6866639319fb04ff23d16589
 
-##
 # visualizing the content of an array
 
 
@@ -63,7 +104,6 @@ def draw():
     cla()
     imshow(c)
 
-##
 # count of upper neighbors of a live cell's Moore neighborhood
 
 
@@ -74,7 +114,6 @@ def number_of_upper_neighbors(x, y):
         # print upper_count
     return upper_count
 
-##
 # count of lower neighbors of a live cell's Moore neighborhood
 
 
@@ -85,7 +124,6 @@ def number_of_lower_neighbors(x, y):
         # print lower_count
     return lower_count
 
-##
 # count of right neighbors of a live cell's Moore neighborhood
 
 
@@ -96,7 +134,6 @@ def number_of_right_neighbors(x, y):
         # print right_count
     return right_count
 
-##
 # count of left neighbors of a live cell's Moore neighborhood
 
 
@@ -107,7 +144,6 @@ def number_of_left_neighbors(x, y):
         # print left_count
     return left_count
 
-##
 # count of von Neumann neighbors of a live cell
 
 
@@ -122,7 +158,6 @@ def number_of_Neumann_neighbors(x, y):
         # print Horizontal_count
     return Vertical_count + Horizontal_count - c[x, y]
 
-##
 # count of Moore neighbors of an alive cell
 
 
@@ -134,28 +169,39 @@ def number_of_Moore_neighbors(x, y):
             # print Moore_count
     return Moore_count - c[x, y]
 
-##
-# step update
-
 
 def step():
+<<<<<<< HEAD
+    global c, nc, array, array0, array1, count, count0, count1, ratio, ratio1, slope0, slope1, delta, o
+    count0 = 0
+    count1 = 0
+    count = 0
+    ratio1 = 0
+    ratio = 0
+    i = 0
+    j = 0
+=======
     global c, nc, array, array0, array1, count0, count1
     count0 = 0
     count1 = 0
+>>>>>>> f1a94baf9f74450a6866639319fb04ff23d16589
     array = []
     array0 = []
     array1 = []
     for x in xrange(L):
         for y in xrange(L):
-            array.append(c[x, y])
-            g = number_of_Moore_neighbors(x, y)
+            if c[x, y] == 1:
+                array.append(c[x, y])
+            g = number_of_Moore_neighbors(x, y) #CA tuning
             if c[x, y] == 0:
-                nc[x, y] = 0 if g <= 6 else 1  # nearest-neightbor parameter
+                nc[x, y] = 0 if g <= 6 else 1
                 array0.append(c[x, y])
             elif c[x, y] == 1:
                 array1.append(c[x, y])
                 for z in range(-1, 2):
-                    # bottleneck configuration due to arrested state
+                    # block generation from randomly distributed points
+                    
+                    #neighbor updating from cell(x,y)
                     m = number_of_upper_neighbors(x, y)
                     if m == 1:
                         nc[x, (y + 1) % L] = 1
@@ -172,23 +218,51 @@ def step():
                     if l == 1 and (m > 1 or n > 1):
                         nc[(x - 1) % L, (y + z) % L] = 0
 
-                    h = number_of_Neumann_neighbors(x, y)
+                    h = number_of_Neumann_neighbors(x, y) #CA tuning
                     if h >= 1:
-                        # nearest-neighbor parameter
                         nc[x, y] = 1 if g <= 6 else 0
 
-                    if g / 8 > (1 - p) * p:  # next-nearest neighbor asymptote
+                    if g / 8 > (1 - p) * p:  # coupling function
                         nc[(x + 1) % L, y] = 1
                     elif g / 8 < (1 - p) * p:
                         nc[(x - 1) % L, y] = 1
                     else:
                         nc[x, y] = 1
+<<<<<<< HEAD
+            i += 1
+            j += g
+    #count = len(array)
+    count0 = len(array0)
+    count1 = len(array1)
+=======
 
     count0 = len(array0)  # total count of cells with value 0 in each step
     count1 = len(array1)  # total count of cells with value 1 in each step
 
+>>>>>>> f1a94baf9f74450a6866639319fb04ff23d16589
     print count1
-
+    o += 1
+    if o == 1:
+        slope0.append(float(count0))
+        slope1.append(float(count1))
+        delta.append(float(j))
+    elif o > 1:
+        slope0.append(float(count0))
+        slope1.append(float(count1))
+        delta.append(float(j))
+        ratio = count0 / float(count1)
+        ratio0 = (j / i)
+        ratio1 = ratio0 / ratio
+        ratio2 = (slope0[o - 1] / float(slope1[o - 1])) - \
+            (slope0[o - 2] / float(slope1[o - 2]))
+        ratio3 = (delta[o - 2] - delta[o - 1]) / i
+        
+        #cotangent generator #for coupling probabilities
+        if ratio1 != 1:
+            print (1 / float(ratio1))
+            #print (ratio3 / float(ratio1 * ratio1) + (1 / float(ratio1)) - ratio2)
+            print (- ratio3 / float(ratio1 * ratio1) - (1 / float(ratio1)) + ratio2)
+            
     c, nc = nc, c
 
 import pycxsimulator
